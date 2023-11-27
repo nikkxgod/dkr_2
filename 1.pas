7f95989fd6433ca8b1ea970c3df5b5e4677b2dda
nikkxgod <100446613+nikkxgod@users.noreply.github.com>
2023-11-27 10:11:24 +0300

    Add files via upload

---
+++ 1.pas
@@ -0,0 +1,22 @@
+﻿var i,x,n,j,flag:integer; A:array [1..20] of integer;
+begin
+  flag:=0;
+  for i:=1 to 20 do begin
+    writeln('Введи ',i,'-й', ' элемент массива');
+    read(x);
+    A[i]:=x;
+  end;
+writeln('Исходный массив: ',A);
+for i:=1 to 20 do begin
+  if flag=1 then break
+  else
+  for j:=i to 20 do begin
+    if A[i]+A[j]>=A[i]*A[j] then begin
+      flag:=1;
+      writeln('Произведение ',A[i],' и ',A[j],' не привышает их сумму');
+      break;
+      end;
+  end;
+  end;
+if flag=0 then writeln('Нет элементов удовлетворяющих условию');
+end.
---
+++ 2.pas
@@ -0,0 +1,26 @@
+﻿var word1,word2,new_str,str:string;i,j,count_word1,count_word2:integer;
+begin
+  writeln('Введите первое слово');
+  readln(word1);
+  writeln('Введите второе слово');
+  readln(word2);
+  writeln('Введите строку');
+  readln(str);
+  for i:=1 to length(str)-length(word1)+1 do begin
+    new_str:='';
+    for j:=i to length(word1)-1+i do begin
+      new_str:=new_str+str[j];
+    end;
+    if new_str=word1 then count_word1:=count_word1+1;
+  end;
+  for i:=1 to length(str)-length(word2)+1 do begin
+    new_str:='';
+    for j:=i to length(word2)-1+i do begin
+      new_str:=new_str+str[j];
+    end;
+    if new_str=word2 then count_word2:=count_word2+1;
+  end;
+  if count_word1>count_word2 then
+    writeln('Первое слово встречается чаще второго')
+  else writeln('Второе слово встречается чаще первое');
+end.
